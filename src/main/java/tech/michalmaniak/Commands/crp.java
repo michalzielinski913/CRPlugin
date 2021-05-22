ac19b60e11a14a5a554eaac301d74a6955feadcb
Michał Zieliński <michalmaniak2000@gmail.com>
2021-05-22 17:35:50 +0200

    CRP command
    User can now define CRP spreadsheet link visible with /crp command.

--- src/main/java/tech/michalmaniak/Commands/crp.java
+++ src/main/java/tech/michalmaniak/Commands/crp.java
@@ -4,18 +4,16 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import tech.michalmaniak.CRPlugin;
 import tech.michalmaniak.Utility.ChatParser;
 
 public class crp implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(sender instanceof Player){
-            Player pl=(Player) sender;
-            pl.sendMessage(ChatParser.prefixColorChat("test"));
-            return true;
-        }
 
-        return false;
+        sender.sendMessage(ChatParser.prefixColorChat("Spreadsheet link: "+ CRPlugin.config.getString("spreadsheet")));
+        return true;
+
     }
 }
